d32f49746ffea9fab504da243adf4eb7948ebced
jk <vargsmane@gmx.de>
2017-11-01 14:20:30 +0100

    Added first classes

--- src/Issue.java
+++ src/Issue.java
@@ -1,5 +1,8 @@
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 enum Priority {
     LOW, NORMAL, HIGH
@@ -9,32 +12,47 @@ public class Issue {
     private String name;
     private Date dateStart;
     private Date dateDone;
-    private Priority priority;
-    private List<Issue> subIssues;
-    private boolean done;
+    private Priority priority = Priority.NORMAL;
+    private List<Issue> subIssues = new ArrayList<>();
+    private boolean done = false;
+    private boolean active = false;
+    private Optional<Issue> requiredIssue = Optional.empty();
 
     public Issue(final String name, final Priority priority, final List<Issue> subIssues) {
         this.name = name;
-        this.dateStart = new Date();
         this.priority = priority;
         this.subIssues = subIssues;
-        this.done = false;
     }
 
-    public String getName() {
-        return name;
+    public Issue(final String name, final Priority priority) {
+        this.name = name;
+        this.priority = priority;
+    }
+
+    public Issue(final String name) {
+        this.name = name;
+    }
+
+    public Issue(final String name, final List<Issue> subIssues) {
+        this.name = name;
+        this.subIssues = subIssues;
+    }
+
+    public void requires(Issue issue) {
+        requiredIssue = Optional.ofNullable(issue);
+    }
+
+    public void start() throws RequirementNotMetException {
+        if (requiredIssue.isPresent()) {
+            active = true;
+            dateStart = new Date();
+        } else {
+            throw new RequirementNotMetException();
+        }
     }
 
     public Date getDateStart() {
         return dateStart;
-    }
-
-    public Date getDateDone() {
-        return dateDone;
-    }
-
-    public Priority getPriority() {
-        return priority;
     }
 
     public List<Issue> getSubIssues() {
@@ -45,8 +63,45 @@ public class Issue {
         return done;
     }
 
-    public void setDone() {
+    public void done() {
         this.done = true;
         this.dateDone = new Date();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(final String name) {
+        this.name = name;
+    }
+
+    public Priority getPriority() {
+        return priority;
+    }
+
+    public void setPriority(final Priority priority) {
+        this.priority = priority;
+    }
+
+    public void addSubIssue(Issue subIssue) {
+        subIssues.add(subIssue);
+    }
+
+    public void removeSubIssue(Issue subIssue) {
+        subIssues.remove(subIssue);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Issue: %s\nPriority: %s\nRequires: %s\nDate started: %s\nDate done: %s\nSub issues: \n%s",
+                name,
+                priority,
+                requiredIssue.isPresent() ? requiredIssue.get().getName() : "None",
+                active ? dateStart.toString() : "Not active yet",
+                done ? dateDone.toString() : "Not done yet",
+                subIssues.isEmpty() ? "None" : subIssues.stream()
+                        .map(Issue::toString)
+                        .collect(Collectors.joining("\n\n")));
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+        Issue findTelephone = new Issue("Find Telephone");
+        Issue dialNumber = new Issue("Dial Number");
+        dialNumber.requires(findTelephone);
+        Issue speak = new Issue("Speak");
+        speak.requires(dialNumber);
+        Task callMary = new Task("Call Mary", Arrays.asList(findTelephone, dialNumber, speak));
+        System.out.println(callMary.toString());
+
+
 
     }
 }
